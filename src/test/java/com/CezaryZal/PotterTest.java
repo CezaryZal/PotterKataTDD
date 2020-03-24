95805279f7991f4f707f66c17ef312fd4eeb1cd4
CezaryZal <czarek0821@gmail.com>
2020-03-24 20:10:28 +0100

    Refactoring PotterTest class
    Create @BeforeEach method to initialization Potter class;
    Take the result to outside from tested methods;

--- src/test/java/com/CezaryZal/PotterTest.java
+++ src/test/java/com/CezaryZal/PotterTest.java
@@ -1,21 +1,29 @@
 package com.CezaryZal;
 
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
 
 public class PotterTest {
 
+    private Potter potter;
+
+    @BeforeEach
+    void setUp() {
+        potter = new Potter();
+    }
+
     @Test
     void shouldReturnPriceOfOneBook() {
-        Potter potter = new Potter();
-        assertThat(potter.buyBooks(1)).isEqualTo("8.0€");
+        String priceOfOneBook = potter.buyBooks(1);
+        assertThat(priceOfOneBook).isEqualTo("8.0€");
     }
 
     @Test
     void shouldReturnPriceOfTwoBook(){
-        Potter potter = new Potter();
-        assertThat(potter.buyBooks(2)).isEqualTo("15.2€");
+        String priceOfTwoBooks = potter.buyBooks(2);
+        assertThat(priceOfTwoBooks).isEqualTo("15.2€");
     }
 }
